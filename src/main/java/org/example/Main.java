5c721e087682f751a2aacce5327cd03207f93cf4
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-09-09 14:17:46 -0500

    cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //programa para crear un ciclo
+        // y crear un menu de opciones
+        Integer opcionSeleccionada=0;
+        while (opcionSeleccionada !=5){
+            System.out.println("Munu de opciones");
+            System.out.println("***********");
+            System.out.println("Digita 1 para sumar ➕");
+            System.out.println("Digita 2 para restar ➖ ");
+            System.out.println("Digita 3 para multiplicar ✖ ");
+            System.out.println("Digita 4 para dividir ➗");
+            System.out.println("Digita 5 para salir del programa ");
+            Scanner leerTeclado = new Scanner(System.in);
+            System.out.println("Selecine una opcion por favor😊");
+            opcionSeleccionada=leerTeclado.nextInt();
         }
     }
 }
